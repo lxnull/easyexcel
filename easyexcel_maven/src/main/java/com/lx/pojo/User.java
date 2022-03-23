04ff00b744c099ee084a7550857b4ef014f08c87
lx <443199068@qq.com>
2022-03-24 00:27:53 +0800

    导出数据，实体类注解 commit

--- easyexcel_maven/src/main/java/com/lx/pojo/User.java
+++ easyexcel_maven/src/main/java/com/lx/pojo/User.java
@@ -1,23 +1,30 @@
 package com.lx.pojo;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.ContentRowHeight;
+import com.alibaba.excel.annotation.write.style.HeadRowHeight;
 import lombok.*;
 import lombok.experimental.Accessors;
 
 import java.util.Date;
 
 @Data
+//@ContentRowHeight// 内容的行高只能放在类上
+//@HeadRowHeight// 表头的行高
 public class User {
 
-    @ExcelProperty("编号")
+    @ExcelProperty(value = "编号",index = 0)
     private String index;
-    @ExcelProperty("状态")
+    @ExcelProperty(value = "状态",index = 1)
     private String status;
-    @ExcelProperty("用户名")
+    @ExcelProperty(value = "用户名",index = 2)
+    @ColumnWidth(15)
     private String name;
-    @ExcelProperty("密码")
+    @ExcelProperty(value = "密码",index = 3)
     private String password;
-    @ExcelProperty("创建日期")
+    @ExcelProperty(value = "创建日期",index = 4)
+    @ColumnWidth(20)
     private Date createTime;
 
 }
--- easyexcel_maven/src/test/java/com/lx/pojo/Test.java
+++ easyexcel_maven/src/test/java/com/lx/pojo/Test.java
@@ -3,9 +3,17 @@ package com.lx.pojo;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.read.builder.ExcelReaderBuilder;
 import com.alibaba.excel.read.builder.ExcelReaderSheetBuilder;
+import com.alibaba.excel.write.builder.ExcelWriterBuilder;
+import com.alibaba.excel.write.builder.ExcelWriterSheetBuilder;
 import com.lx.listener.UserListener;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Test {
+
+    static String PATH = "G:\\JavaSpace\\Java进阶\\easyexcel\\";
 
     @org.junit.Test
     public void test() {
@@ -17,12 +25,42 @@ public class Test {
          * Returns:
          * Excel reader builder.
          */
-        String path = "G:\\JavaSpace\\Java进阶\\easyexcel\\a.xlsx";
         // 获取工作簿对象
-        ExcelReaderBuilder workbook = EasyExcel.read(path, User.class, new UserListener());
+        ExcelReaderBuilder workbook = EasyExcel.read(PATH + "a.xlsx", User.class, new UserListener());
         // 获取工作表
         ExcelReaderSheetBuilder sheet = workbook.sheet();
         // 读取工作表中的内容
         sheet.doRead();
+    }
+
+    @org.junit.Test
+    public void testWrite() {
+        /*
+         * Build excel the write 创建工作簿
+         * Params:
+         * file – 要写入的文件路径
+         * head – 写入要导入excel的类的.class文件
+         * Returns:
+         * Excel writer builder
+         * * */
+        ExcelWriterBuilder writeWorkbook = EasyExcel.write(PATH + "b.xlsx", User.class);
+        // 工作表对象
+        ExcelWriterSheetBuilder sheet = writeWorkbook.sheet();
+        // 写
+        sheet.doWrite(findAll());
+    }
+
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setName("张三_" + i);
+            user.setIndex(String.valueOf(i));
+            user.setCreateTime(new Date());
+            user.setPassword(String.valueOf((int)((Math.random()*9+1)*100000)));
+            user.setStatus(i % 2 == 0 ? "启用" : "停用");
+            users.add(user);
+        }
+        return users;
     }
 }
